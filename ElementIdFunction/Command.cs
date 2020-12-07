4fc9417e94a54c092dbc434d6ad12686a186fc5f
Luca Della Bianca <lucadellabianca4@gmail.com>
2020-12-07 18:22:48 +0100

    Add project files.

---
+++ ElementIdFunction/Command.cs
@@ -0,0 +1,116 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ElementIdFunction
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        #region Private data members
+        Autodesk.Revit.DB.Element m_slab = null;      // Store the selected element
+        private ArrayList elementsList;               // Store the list of the elements selected
+        #endregion
+
+
+        #region class public property
+        /// <summary>
+        /// With the selected elements, export the list of all its id
+        /// </summary>
+        public ArrayList Elements
+        {
+            get
+            {
+                return elementsList;
+            }
+        }
+        #endregion
+
+
+        #region class public method
+        /// <summary>
+        /// Default constructor of Command
+        /// </summary>
+        public Command()
+        {
+            // Construct the data members for the property
+            elementsList = new ArrayList();
+        }
+        #endregion
+
+
+        #region Interface implemetation
+        /// <summary>
+        /// Implement this method as an external command for Revit.
+        /// </summary>
+        /// <param name="commandData">An object that is passed to the external application 
+        /// which contains data related to the command, 
+        /// such as the application object and active view.</param>
+        /// <param name="message">A message that can be set by the external application 
+        /// which will be displayed if a failure or cancellation is returned by 
+        /// the external command.</param>
+        /// <param name="elements">A set of elements to which the external application 
+        /// can add elements that are to be highlighted in case of failure or cancellation.</param>
+        /// <returns>Return the status of the external command. 
+        /// A result of Succeeded means that the API external method functioned as expected. 
+        /// Cancelled can be used to signify that the user cancelled the external operation 
+        /// at some point. Failure should be returned if the application is unable to proceed with 
+        /// the operation.</returns>
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication revit = commandData.Application;
+
+            // Get the selected view
+            UIDocument uidoc = revit.ActiveUIDocument;
+            Selection choices = uidoc.Selection;
+            ElementSet collection = new ElementSet();
+            foreach (ElementId elementId in choices.GetElementIds())
+            {
+                collection.Insert(uidoc.Document.GetElement(elementId));
+            }
+
+            // If the item is not selected, it prompts you to select at least one
+            foreach (Element e in collection)
+            {
+                m_slab = e as Element;
+                if (null == m_slab)
+                {
+                    message = "Please select an element.";
+                    return Autodesk.Revit.UI.Result.Failed;
+                }
+            }
+
+            // Get all elements id
+            foreach (Element e in collection)
+            {
+                // With the element selected, judge if the Id of the element exists, 
+                // if it doesn't exist, it should be zero.                
+                if (e.Id == null)
+                {
+                    elementsList.Add("No element");
+                }
+                else
+                {
+                    elementsList.Add(e.Id.ToString());
+                }
+
+            }
+
+            // Display them in a form
+            ElementIdFunctionWF displayForm = new ElementIdFunctionWF(this);
+            displayForm.ShowDialog();
+
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+        #endregion
+    }
+}
---
+++ ElementIdFunction/ElementIdFunctionWF.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace ElementIdFunction
+{
+    partial class ElementIdFunctionWF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.functionGroupBox = new System.Windows.Forms.GroupBox();
+            this.functionListBox = new System.Windows.Forms.ListBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.functionGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // functionGroupBox
+            // 
+            this.functionGroupBox.Controls.Add(this.okButton);
+            this.functionGroupBox.Controls.Add(this.functionListBox);
+            this.functionGroupBox.Location = new System.Drawing.Point(13, 13);
+            this.functionGroupBox.Name = "functionGroupBox";
+            this.functionGroupBox.Size = new System.Drawing.Size(302, 449);
+            this.functionGroupBox.TabIndex = 0;
+            this.functionGroupBox.TabStop = false;
+            this.functionGroupBox.Text = "List of ElementsID";
+            // 
+            // functionListBox
+            // 
+            this.functionListBox.FormattingEnabled = true;
+            this.functionListBox.ItemHeight = 16;
+            this.functionListBox.Location = new System.Drawing.Point(7, 22);
+            this.functionListBox.Name = "functionListBox";
+            this.functionListBox.Size = new System.Drawing.Size(289, 388);
+            this.functionListBox.TabIndex = 0;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(221, 416);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 1;
+            this.okButton.Text = "Close";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // ElementIdFunctionWF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 474);
+            this.Controls.Add(this.functionGroupBox);
+            this.CancelButton = this.okButton;
+            this.Name = "ElementIdFunctionWF";
+            this.Text = "ElementIdFunctionWF";
+            this.MaximizeBox = false;
+            this.MinimizeBox = true;
+            this.ShowInTaskbar = false;
+            this.functionGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox functionGroupBox;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.ListBox functionListBox;
+    }
+}
